4bdaa1bd446b88db3f51f2acfea9d28f0567e609
amoldattu <amoldattu2012@gmail.com>
2024-08-29 04:58:55 +0530

    Update app.py and changed the message

--- day-14/simple-python-app/app.py
+++ day-14/simple-python-app/app.py
@@ -4,9 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world!'
+    return 'Hi this is amol dattu and how are you!'
     
-
 if __name__ == '__main__':
     app.run()
 
