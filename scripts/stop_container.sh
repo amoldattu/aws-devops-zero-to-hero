ceb1f00ae19e54859ebdc398211fbd96e6300211
amoldattu <amoldattu2012@gmail.com>
2024-08-30 04:27:17 +0530

    Update stop_container.sh id

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,5 +2,5 @@
 set -e
 
 # Stop the running container (if any)
-containerid=`docker ps | awk -F “ ” ‘{print $1}’`
-docker rm -f $containerid
+659b8347da0b =`docker ps | awk -F “ ” ‘{print $1}’`
+docker rm -f 659b8347da0b 
